fa7c349cf21cab2598eb272f11cb1b5b10bee1f8
Lukas Kratochvila <lukas.kratochvila@datasentics.com>
2025-03-31 10:57:16 +0200

    add comment

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def get_first_day_of_month(dt: datetime.datetime) -> datetime.datetime:
 
 
 def get_first_day_of_next_month(dt: datetime.datetime) -> datetime.datetime:
-    next_month_dt = dt + relativedelta(months=1)
+    next_month_dt = dt + relativedelta(months=1) # works even for Jan and Dec
     return next_month_dt.replace(day=1)
 
 
